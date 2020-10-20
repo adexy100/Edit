70b8e05d26bfb8675b79f5344fbc47a502d44315
unknown <adexy100@gmail.com>
2020-10-20 10:52:54 -1200

    initial

---
+++ frontend/build/precache-manifest.da33a5132668a37a1e6bf93ea33aa4f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0731954b1e214ff4fda9e020bd45c7bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be06e43412cf7ad89e7b",
+    "url": "/static/css/main.62b7980e.chunk.css"
+  },
+  {
+    "revision": "74f77146fc36a4722793",
+    "url": "/static/js/2.2ffe1190.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.2ffe1190.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be06e43412cf7ad89e7b",
+    "url": "/static/js/main.77b91c49.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
